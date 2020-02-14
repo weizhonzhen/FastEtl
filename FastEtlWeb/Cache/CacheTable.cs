48043092c2592ccae8830522d1c1838e98ebc111
weizhonzhen <czdswzz@126.com>
2020-02-14 15:46:40 +0800

    Add files via upload

---
+++ FastEtlWeb/Cache/CacheTable.cs
@@ -0,0 +1,20 @@
+﻿namespace FastEtlWeb.Cache
+{
+    public class CacheTable
+    {
+        /// <summary>
+        /// 显示表名
+        /// </summary>
+        public string ShowName { get; set; }
+
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Comments { get; set; }
+    }
+}
