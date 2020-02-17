a47535ebd13db6dad239e003a74cae6240cd82ae
weizhonzhen <czdswzz@126.com>
2020-02-17 10:20:40 +0800

    Add files via upload

---
+++ FastEtlService.core/Config/AppEtl.cs
@@ -0,0 +1,60 @@
+﻿public static class AppEtl
+{
+    public static string Db = "Etl";
+
+    public class DataDbType
+    {
+        /// <summary>
+        /// oracle
+        /// </summary>
+        public readonly static string Oracle = "Oracle";
+
+        /// <summary>
+        /// mysql
+        /// </summary>
+        public readonly static string MySql = "MySql";
+
+        /// <summary>
+        /// sql server
+        /// </summary>
+        public readonly static string SqlServer = "SqlServer";
+    }
+
+    public class Provider
+    {
+        /// <summary>
+        /// oracle
+        /// </summary>
+        public readonly static string Oracle = "Oracle.ManagedDataAccess";
+
+        /// <summary>
+        /// mysql
+        /// </summary>
+        public readonly static string MySql = "MySql.Data";
+
+        /// <summary>
+        /// sql server
+        /// </summary>
+        public readonly static string SqlServer = "System.Data";
+    }
+
+    public class FactoryClient
+    {
+        /// <summary>
+        /// oracle
+        /// </summary>
+        public readonly static string Oracle = "Oracle.ManagedDataAccess.Client.OracleClientFactory";
+
+        /// <summary>
+        /// mysql
+        /// </summary>
+        public readonly static string MySql = "MySql.Data.MySqlClient.MySqlClientFactory";
+
+
+        /// <summary>
+        /// sql server
+        /// </summary>
+        public readonly static string SqlServer = "System.Data.SqlClient.SqlClientFactory";
+    }
+}
+
