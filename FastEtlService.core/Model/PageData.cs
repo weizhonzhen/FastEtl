eb0a1c3358d829eca51c0ff4ef9a76da71b68791
weizhonzhen <czdswzz@126.com>
2020-02-21 09:20:59 +0800

    Add files via upload

---
+++ FastEtlService.core/Model/PageData.cs
@@ -0,0 +1,13 @@
+﻿using FastUntility.Core.Page;
+using System.Collections.Generic;
+
+namespace FastEtlService.core.Model
+{
+    /// <summary>
+    /// 分页数据
+    /// </summary>
+    public class PageData : PageModel
+    {
+        public List<Dictionary<string, object>> list { set; get; } = new List<Dictionary<string, object>>();
+    }
+}
