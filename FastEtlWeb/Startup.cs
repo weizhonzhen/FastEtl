7da3ede0691e6106ac53d2b6f7bdb4ec547b51cc
weizhonzhen <czdswzz@126.com>
2020-04-28 09:42:05 +0800

    Update Startup.cs

--- FastEtlWeb/Startup.cs
+++ FastEtlWeb/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
+using System.Collections.Generic;
 
 namespace FastEtlWeb
 {
