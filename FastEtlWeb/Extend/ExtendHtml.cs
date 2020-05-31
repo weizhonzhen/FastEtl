f8963f8577d559e055a41a51b4ebb68593589f09
weizhonzhen <czdswzz@126.com>
2020-05-31 22:17:28 +0800

    Add files via upload

--- FastEtlWeb/Extend/ExtendHtml.cs
+++ FastEtlWeb/Extend/ExtendHtml.cs
@@ -45,7 +45,7 @@ namespace Microsoft.AspNetCore.Mvc.Rendering
         public static IHtmlContent DropDownListForDic<TModel, TProperty>(this IHtmlHelper<TModel> html, Expression<Func<TModel, TProperty>> expression, object htmlAttributes,object selectValue)
         {
             var typeList = new List<SelectListItem>();
-            var list = FastRead.Query<Data_Dic>(a => a.Id != "").ToList<Data_Dic>();
+            var list = FastRead.Query<Data_Dic>(a => a.Id != "", null, AppEtl.Db).ToList<Data_Dic>();
 
             foreach (var item in list)
             {
@@ -69,7 +69,7 @@ namespace Microsoft.AspNetCore.Mvc.Rendering
         public static IHtmlContent DropDownListForDic(this IHtmlHelper html,string name, object htmlAttributes,bool isAll=false)
         {
             var typeList = new List<SelectListItem>();
-            var list = FastRead.Query<Data_Dic>(a => a.Id != "").ToList<Data_Dic>();
+            var list = FastRead.Query<Data_Dic>(a => a.Id != "", null, AppEtl.Db).ToList<Data_Dic>();
 
             if(isAll)
                 typeList.Add(new SelectListItem { Text = "全部", Value = "" });
@@ -80,6 +80,28 @@ namespace Microsoft.AspNetCore.Mvc.Rendering
             }
            
             return html.DropDownList(name, typeList, htmlAttributes);
+        }
+
+        /// <summary>
+        /// 数据源
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="html"></param>
+        /// <param name="expression"></param>
+        /// <param name="htmlAttributes"></param>
+        /// <returns></returns>
+        public static IHtmlContent DropDownListForDataSource<TModel, TProperty>(this IHtmlHelper<TModel> html, Expression<Func<TModel, TProperty>> expression, object htmlAttributes)
+        {
+            var typeList = new List<SelectListItem>();
+            var list = FastRead.Query<Data_Source>(a => a.Id != "", null, AppEtl.Db).ToList<Data_Source>();
+
+            foreach (var item in list)
+            {
+                typeList.Add(new SelectListItem { Text = string.Format("{0}({1}:{2})",item.LinkName,item.Host,item.Port), Value = item.Id });
+            }
+
+            return html.DropDownListFor(expression, typeList, htmlAttributes);
         }
     }
 }
